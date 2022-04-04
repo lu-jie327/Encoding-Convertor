fe2a42cc904edcb65da83afa02a0255e8ada85fe
lu-jie327 <102394981+lu-jie327@users.noreply.github.com>
2022-04-04 11:20:12 +0800

    Add files via upload

---
+++ Ribbon2.Designer.vb
@@ -0,0 +1,116 @@
+﻿Partial Class Ribbon2
+    Inherits Microsoft.Office.Tools.Ribbon.RibbonBase
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New(ByVal container As System.ComponentModel.IContainer)
+        MyClass.New()
+
+        'Required for Windows.Forms Class Composition Designer support
+        If (container IsNot Nothing) Then
+            container.Add(Me)
+        End If
+
+    End Sub
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New()
+        MyBase.New(Globals.Factory.GetRibbonFactory())
+
+        'This call is required by the Component Designer.
+        InitializeComponent()
+
+    End Sub
+
+    'Component overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Component Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Component Designer
+    'It can be modified using the Component Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Tab1 = Me.Factory.CreateRibbonTab
+        Me.Group1 = Me.Factory.CreateRibbonGroup
+        Me.UTF8Btn = Me.Factory.CreateRibbonButton
+        Me.UTF16LEBtn = Me.Factory.CreateRibbonButton
+        Me.UTF16BEBtn = Me.Factory.CreateRibbonButton
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Tab1.SuspendLayout()
+        Me.Group1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Tab1
+        '
+        Me.Tab1.Groups.Add(Me.Group1)
+        Me.Tab1.Label = "Encoding"
+        Me.Tab1.Name = "Tab1"
+        '
+        'Group1
+        '
+        Me.Group1.Items.Add(Me.UTF8Btn)
+        Me.Group1.Items.Add(Me.UTF16LEBtn)
+        Me.Group1.Items.Add(Me.UTF16BEBtn)
+        Me.Group1.Label = "Convertor"
+        Me.Group1.Name = "Group1"
+        '
+        'UTF8Btn
+        '
+        Me.UTF8Btn.Label = "To UTF-8"
+        Me.UTF8Btn.Name = "UTF8Btn"
+        '
+        'UTF16LEBtn
+        '
+        Me.UTF16LEBtn.Label = "To UTF-16 LE"
+        Me.UTF16LEBtn.Name = "UTF16LEBtn"
+        '
+        'UTF16BEBtn
+        '
+        Me.UTF16BEBtn.Label = "To UTF-16 BE"
+        Me.UTF16BEBtn.Name = "UTF16BEBtn"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Ribbon2
+        '
+        Me.Name = "Ribbon2"
+        Me.RibbonType = "Microsoft.Excel.Workbook"
+        Me.Tabs.Add(Me.Tab1)
+        Me.Tab1.ResumeLayout(False)
+        Me.Tab1.PerformLayout()
+        Me.Group1.ResumeLayout(False)
+        Me.Group1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Tab1 As Microsoft.Office.Tools.Ribbon.RibbonTab
+    Friend WithEvents Group1 As Microsoft.Office.Tools.Ribbon.RibbonGroup
+    Friend WithEvents UTF16LEBtn As Microsoft.Office.Tools.Ribbon.RibbonButton
+    Friend WithEvents UTF16BEBtn As Microsoft.Office.Tools.Ribbon.RibbonButton
+    Friend WithEvents OpenFileDialog1 As Windows.Forms.OpenFileDialog
+    Friend WithEvents UTF8Btn As Microsoft.Office.Tools.Ribbon.RibbonButton
+End Class
+
+Partial Class ThisRibbonCollection
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Friend ReadOnly Property Ribbon2() As Ribbon2
+        Get
+            Return Me.GetRibbon(Of Ribbon2)()
+        End Get
+    End Property
+End Class
---
+++ Ribbon2.vb
@@ -0,0 +1,140 @@
+﻿Imports Microsoft.Office.Tools.Ribbon
+Imports System.IO
+
+Public Class Ribbon2
+
+    Private Sub Ribbon2_Load(ByVal sender As System.Object, ByVal e As RibbonUIEventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Function Get_ActiveBook_FullName() As String
+
+        Get_ActiveBook_FullName = Globals.ThisAddIn.Application.ActiveWorkbook.FullName
+
+    End Function
+
+    Private Sub Format_Convert(ByRef filestr As String, ByRef fileToWrite As String, ByVal encoding As Encoding)
+        Dim fws As FileStream
+        Dim fw As StreamWriter
+
+        fws = New FileStream(fileToWrite, FileMode.Create)
+        fw = New StreamWriter(fws, encoding)
+        fw.Write(filestr)
+        fw.Dispose()
+        If Not (fws Is Nothing) Then
+            fws.Dispose()
+            MsgBox(fileToWrite + " was created successfully.", MsgBoxStyle.Information, "Encoding Convertor")
+        End If
+    End Sub
+
+    Private Sub UTF16LEBtn_Click(sender As Object, e As RibbonControlEventArgs) Handles UTF16LEBtn.Click
+        Dim frs As FileStream
+        Dim fr As StreamReader
+        Dim filestr As String = ""
+
+        Dim fileFullName As String = Get_ActiveBook_FullName()
+        Dim slashPos As Int16 = fileFullName.LastIndexOf("\")
+
+        If fileFullName Is Nothing Then
+            OpenFileDialog1.FileName = "*.*"
+        Else
+            If slashPos > 0 Then
+                OpenFileDialog1.InitialDirectory = fileFullName.Substring(0, slashPos)
+                OpenFileDialog1.FileName = fileFullName.Substring(slashPos + 1)
+            Else
+                OpenFileDialog1.FileName = fileFullName
+            End If
+        End If
+
+        If OpenFileDialog1.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
+            'Set FileStream as FileShare.ReadWrite in case the file has been opened by another process. 
+            frs = New FileStream(OpenFileDialog1.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+            fr = New StreamReader(frs)
+            filestr = fr.ReadToEnd()
+            fr.Dispose()
+            Format_Convert(filestr, OpenFileDialog1.FileName + ".Unicode.txt", System.Text.Encoding.Unicode)
+
+            'Dim fr1 As FileStream
+            'fr1 = File.OpenRead(OpenFileDialog1.FileName)
+
+
+            'Dim r As BinaryReader = New BinaryReader(fr1, System.Text.Encoding.Default)
+            'Dim ss() As Byte = r.ReadBytes(3)
+            'fr1.Close()
+
+            'Dim encoding_str As String = ""
+
+            'If (ss(0) >= 239) Then
+            '    encoding_str = "utf-8"
+            '    If (ss(0) = 254) And (ss(1) = 255) Then
+            '        encoding_str = "utf-16 BE"
+            '    ElseIf (ss(0) = 255) And (ss(1) = 254) Then
+            '        encoding_str = "utf-16 LE"
+            '    Else
+            '        encoding_str = "default"
+            '    End If
+            'Else
+            '    encoding_str = "default"
+            'End If
+
+            'MsgBox(encoding_str)
+
+        End If
+    End Sub
+
+    Private Sub UTF16BEBtn_Click(sender As Object, e As RibbonControlEventArgs) Handles UTF16BEBtn.Click
+        Dim frs As FileStream
+        Dim fr As StreamReader
+        Dim filestr As String = ""
+
+        Dim fileFullName As String = Get_ActiveBook_FullName()
+        Dim slashPos As Int16 = fileFullName.LastIndexOf("\")
+
+        If fileFullName Is Nothing Then
+            OpenFileDialog1.FileName = "*.*"
+        Else
+            If slashPos > 0 Then
+                OpenFileDialog1.InitialDirectory = fileFullName.Substring(0, slashPos)
+                OpenFileDialog1.FileName = fileFullName.Substring(slashPos + 1)
+            Else
+                OpenFileDialog1.FileName = fileFullName
+            End If
+        End If
+
+        If OpenFileDialog1.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
+            frs = New FileStream(OpenFileDialog1.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+            fr = New StreamReader(frs)
+            filestr = fr.ReadToEnd()
+            fr.Dispose()
+            Format_Convert(filestr, OpenFileDialog1.FileName + ".BEUnicode.txt", System.Text.Encoding.BigEndianUnicode)
+        End If
+    End Sub
+
+    Private Sub UTF8Btn_Click(sender As Object, e As RibbonControlEventArgs) Handles UTF8Btn.Click
+        Dim frs As FileStream
+        Dim fr As StreamReader
+        Dim filestr As String = ""
+
+        Dim fileFullName As String = Get_ActiveBook_FullName()
+        Dim slashPos As Int16 = fileFullName.LastIndexOf("\")
+
+        If fileFullName Is Nothing Then
+            OpenFileDialog1.FileName = "*.*"
+        Else
+            If slashPos > 0 Then
+                OpenFileDialog1.InitialDirectory = fileFullName.Substring(0, slashPos)
+                OpenFileDialog1.FileName = fileFullName.Substring(slashPos + 1)
+            Else
+                OpenFileDialog1.FileName = fileFullName
+            End If
+        End If
+
+        If OpenFileDialog1.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
+            frs = New FileStream(OpenFileDialog1.FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+            fr = New StreamReader(frs)
+            filestr = fr.ReadToEnd()
+            fr.Dispose()
+            Format_Convert(filestr, OpenFileDialog1.FileName + ".UTF8.txt", System.Text.Encoding.UTF8)
+        End If
+    End Sub
+End Class
